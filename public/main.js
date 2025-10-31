0a71b94a75748dbe70005eb1316a4c4550622de8
laivandai123 <laivandai012@gmail.com>
2025-10-31 15:58:08 +0700

    responsive header

---
+++ public/main.js
@@ -0,0 +1,14 @@
+const topMenu = document.getElementById("ct-top-menu");
+const toggleTopMenuIcon = document.getElementById("ct-toggle-top-menu-icon");
+
+document.addEventListener("click", (e) => {
+    if (toggleTopMenuIcon.contains(e.target)) {
+        topMenu.classList.toggle("ct-top-menu-expanded");
+        topMenu.classList.toggle("hidden");
+    } else {
+        if (topMenu.classList.contains("ct-top-menu-expanded")) {
+            topMenu.classList.remove("ct-top-menu-expanded");
+            topMenu.classList.add("hidden");
+        }
+    }
+});
